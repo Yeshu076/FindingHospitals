a0addeb16c4b0e74fc5ef5b81083048712192c88
Yeshwanth_Jaisamraj <yeshwanth.yeshu07@gmail.com>
2022-05-13 08:50:41 +0530

    First Stage of Code for CTS Hackathon Project

---
+++ FindingHospitals/src/main/java/pages/CorporateWellness.java
@@ -0,0 +1,93 @@
+package pages;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import base.Base;
+
+public class CorporateWellness extends Base {
+	WebElement Name, OrgName, Email, Contact;
+
+	By providers = By.xpath("//span[normalize-space()='Wellness Plans']");
+	By name = By.xpath("//*[@id='name']");
+	By orgName = By.xpath("//*[@id='organizationName']");
+	By email = By.xpath("//header[@id='header']//input[@id='officialEmailId']");
+	By contact = By.xpath("//header[@id='header']//input[@id='contactNumber']");
+	By button = By
+			.xpath("//header[@id='header']//button[contains(@type,'submit')][normalize-space()='Schedule a demo']");
+	By organizationSize = By.xpath("//*[@id='organizationSize']");
+	By dropDown = By.xpath("//option[text()='<500']");
+
+	@SuppressWarnings("resource")
+	public void formFill() throws InterruptedException, IOException {
+
+		logger = report.createTest("Corporate Wellness");
+
+		// Initializing the Excel Sheet
+		FileInputStream fs = new FileInputStream(System.getProperty("user.dir") + "/src/test/resources/TestData.xlsx");
+		XSSFWorkbook workbook = new XSSFWorkbook(fs);
+		XSSFSheet sheet = workbook.getSheet("Data");
+
+		// Selecting the corporate option
+		try {
+			findElement(providers).click();
+			// findElement(corporate).click();
+			reportPass("Corporate Wellness Link Clicked Successfully");
+		} catch (Exception e) {
+			reportFail(e.getMessage());
+		}
+
+		// Switching to new tab
+		try {
+			Set<String> currentHandle = driver.getWindowHandles();
+			Iterator<String> iterator = currentHandle.iterator();
+			iterator.next();
+			String corporate = iterator.next();
+			driver.switchTo().window(corporate);
+			reportPass("Switched to new tab Successfully");
+		} catch (Exception e) {
+			reportFail(e.getMessage());
+		}
+
+		// Filling the form
+		try {
+			Name = driver.findElement(name);
+			OrgName = driver.findElement(orgName);
+			Email = driver.findElement(email);
+			Contact = driver.findElement(contact);
+
+		//	Name.sendKeys(sheet.getRow(3).getCell(0).getStringCellValue());
+		//	OrgName.sendKeys(sheet.getRow(3).getCell(1).getStringCellValue());
+		//	Contact.sendKeys("" + (long) sheet.getRow(3).getCell(3).getNumericCellValue());
+		//	Email.sendKeys(sheet.getRow(3).getCell(2).getStringCellValue()); 
+			
+			Name.sendKeys(sheet.getRow(2).getCell(0).getStringCellValue());
+			OrgName.sendKeys(sheet.getRow(2).getCell(1).getStringCellValue());
+			Contact.sendKeys("" + (long) sheet.getRow(2).getCell(3).getNumericCellValue());
+			Email.sendKeys(sheet.getRow(2).getCell(2).getStringCellValue());
+
+			driver.findElement(organizationSize).click();
+			driver.findElement(dropDown).click();
+		//	reportPass("Data entered successfully");
+			reportPass("Schedule a demo button is not activated due to filling invalid details");
+
+			Screenshot("dataEntered");
+	        Thread.sleep(2000);
+	    //
+		//
+		//
+		//
+		} catch (Exception e) {
+			reportFail(e.getMessage());
+		}
+	}
+
+}
