fec779703b58d72ece931336c97fea00cacc223f
Yeshwanth_Jaisamraj <yeshwanth.yeshu07@gmail.com>
2022-05-17 20:18:04 +0530

    updated

--- FindingHospitals/src/main/java/base/Base.java
+++ FindingHospitals/src/main/java/base/Base.java
@@ -11,12 +11,14 @@ import java.util.concurrent.TimeUnit;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -72,8 +74,8 @@ public class Base {
 
 	}
 
-	// To call different browsers using Selenium Grid
-	
+//	// To call different browsers using Selenium Grid
+//	
 //	public void invokeBrowser() throws MalformedURLException {
 //		prop = new Properties();
 //		String nodeURL = "http://192.168.56.1:4444/wd/hub";
